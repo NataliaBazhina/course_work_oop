0fe90ad2407d6757d2ba1ced52de201b032f4a7e
Natalia Bazhina <nataliaagapova27@yandex.ru>
2024-07-28 12:39:27 +0300

    add class JSONsaver

---
+++ src/JSONsaver.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+import json
+from json import JSONDecodeError
+from src.vacancies import Vacancy
+import os
+
+class Saver(ABC):
+    """Абстрактный класс для работы с файлом"""
+    @abstractmethod
+    def add_vacancy(self, vacancy: Vacancy):
+        pass
+
+    @abstractmethod
+    def get_vacancies_by_keywords(self, keywords):
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self, title):
+        pass
+
+class JSONsaver(Saver):
+
+    def __init__(self,file_path):
+        self.file_path = file_path
+        self.vacancies = self._load_vacancies()
+
+    def add_vacancy(self, vacancy: Vacancy):
+        """добавляет вакансию в файл"""
+        vac = {
+            "title": vacancy.title,
+            "link": vacancy.link,
+            "salary": vacancy.salary,
+            "description": vacancy.description,
+            "requirements": vacancy.requirements
+        }
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w', encoding="utf-8") as f:
+                json.dump([], f)
+        self.vacancies.append(vac)
+        with open(self.file_path, 'w', encoding="utf-8") as f:
+            json.dump(self.vacancies, f, ensure_ascii=False, indent=4)
+        print(f"Вакансия '{vacancy.title}' добавлена")
+
+
+    def delete_vacancy(self, title):
+        """Удаляет вакансию с указанным заголовком из файла"""
+        if not os.path.exists(self.file_path):
+            print("Файл не найден.")
+            return
+        self.vacancies = [vacancy for vacancy in self.vacancies if vacancy["title"] != title]
+        with open(self.file_path, 'w', encoding="utf-8") as f:
+            json.dump(self.vacancies, f, ensure_ascii=False, indent=4)
+        print(f"Вакансия '{title}' удалена из файла.")
+
+
+    def get_vacancies_by_keywords(self, keywords):
+        """Отбирает вакансии по ключевым словам в описании"""
+        filtered_vacancies = []
+        for vacancy in self.vacancies:
+            if 'description' in vacancy and vacancy['description']:
+                description = vacancy['description'].lower()
+                for word in keywords.split():
+                    if word in description:
+                        filtered_vacancies.append(vacancy)
+                        break
+        return filtered_vacancies
+
+    def _load_vacancies(self):
+        try:
+            with open(self.file_path, 'r') as file:
+                data = json.load(file)
+                if data is not None:
+                    return data
+                else:
+                    return []
+        except FileNotFoundError:
+            return []
+        except JSONDecodeError:
+            return []
+
+
+jsonsaver = JSONsaver("file_path.json")
+vacancy = Vacancy("Программист","http://example.com/job1",60000,"Разработка приложений на Python","Опыт работы 2 года")
+vacancy1 = Vacancy("Аналитик", "http://example.com/job2", 50000, "Анализ данных", "Знание SQL")
+vacancy2 = Vacancy("Дизайнер", "http://example.com/job3", 55000, "Дизайн интерфейсов", "Навыки работы с Figma")
+
+jsonsaver.add_vacancy(vacancy)
+jsonsaver.add_vacancy(vacancy1)
+
+jsonsaver.delete_vacancy("Аналитик")
+jsonsaver.add_vacancy(vacancy2)
+
+vacancies = [vacancy, vacancy1, vacancy2]
+keyword = "интерфейс"
+filtered_vacancies = jsonsaver.get_vacancies_by_keywords(keyword)
+print("Отфильтрованные вакансии:")
+for vacancy in filtered_vacancies:
+    print(vacancy['title'])
--- src/vacancies.py
+++ src/vacancies.py
@@ -1,7 +1,4 @@
-from abc import ABC, abstractmethod
-from api import HH
 import json
-import os
 
 class Vacancy:
     """Класс для представления информации о вакансии."""
@@ -29,71 +26,17 @@ class Vacancy:
         """Представление информации о вакансии в виде строки."""
         return f"Vacancy(title={self.title}, link={self.link}, salary={self.salary}, description={self.description}, requirements={self.requirements})"
 
-    def add_vacancy(self,file_path):
-        """добавляет вакансию в файл"""
-        vac = {
-            "title": self.title,
-            "link": self.link,
-            "salary": self.salary,
-            "description": self.description,
-            "requirements": self.requirements
-        }
-        if not os.path.exists(file_path):
-            with open(file_path, 'w', encoding="utf-8") as f:
-                json.dump([], f)
-        with open(file_path, 'r', encoding="utf-8") as f:
-            vacancies = json.load(f)
-        vacancies.append(vac)
-        with open(file_path, 'w', encoding="utf-8") as f:
-            json.dump(vacancies, f, indent=4)
-        print(f"Вакансия добавлена") #добавить название вакансии!
-
-    def delete_vacancy(self, file_path, title):
-            """Удаляет вакансию с указанным заголовком из файла"""
-            if not os.path.exists(file_path):
-                print("Файл не найден.")
-                return
-            with open(file_path, 'r', encoding="utf-8") as f:
-                vacancies = json.load(f)
-            vacancies = [vacancy for vacancy in vacancies if vacancy["title"] != title]
-
-            with open(file_path, 'w', encoding="utf-8") as f:
-                json.dump(vacancies, f, indent=4)
-            print(f"Вакансия '{title}' удалена из файла.")
-
-    def get_vacancies_by_criteria(vacancies, criteria):
-        """отбирает вакансии по ключевым словам в описании"""
-        return [v for v in vacancies if v.description and any(word in v.description.lower() for word in criteria)]
-
-    # def save_vacancies(self):
-    #     """ сохраняет вакансии в файл """
-    #     with open(self.file_path, 'w', encoding="utf-8") as f:
-    #         json.dump(self.vacancies, f, indent=4)
 
 
-class VacancyFile(ABC):
-    """Класс для работы с файлом"""
-    @abstractmethod
-    def add_vacancy(self, vacancy: Vacancy):
-        pass
+    def save_vacancies(self):
+        """ сохраняет вакансии в файл """
+        with open(self.file_path, 'w', encoding="utf-8") as f:
+            json.dump(self.vacancies, f, ensure_ascii=False, indent=4)
 
-    @abstractmethod
-    def get_vacancies_by_criteria(self, filename,criteria):
-        pass
 
-    @abstractmethod
-    def delete_vacancy(self, vacancy: Vacancy):
-        pass
+
 
 # hh = HH()
 # hh.get_vacancies("python")
 # print(hh.vacancies)
-
-vacancy = Vacancy("jgg","hjn",12,"jhg","jg")
-vacancy1 = Vacancy("jhhgf","kyf",60, "hg","jgf")
-vacancy2 = Vacancy("khg","jg",90,"khg","jgfg")
-vacancy.add_vacancy("kjm.json")
-vacancy1.add_vacancy("kjm.json")
-vacancy2.add_vacancy("kjm.json")
-vacancy2.delete_vacancy("kjm.json","khg")
 
